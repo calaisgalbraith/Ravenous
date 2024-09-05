5900692ab5fe139f884efff4fe7c279c7fc5a5d1
“calaisgalbraith” <calaisgalbraith@gmail.com>
2024-09-04 20:23:45 -0600

    added styling & state hooks to SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,37 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Buisness from '../Business/Business'
 import './SearchBar.css'
 
 const searchOptions = {
-    "Best Match": "best_match",
-    "Highest Rated": "rating",
-    "Most Reviewed": "review_count"
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
 }
 
-const searchBar = () => {
-
+const SearchBar = () => {
     const searchByOptions= () => {
         return Object.keys(searchOptions).map((searchByOption) => 
-            <li key={searchOptions[searchByOption]} className="nav-item nav-link searchByOption">{searchByOption}
+            <li
+                key={searchOptions[searchByOption]}
+                className='nav-item nav-link searchByOption'
+                onClick={() => {
+                    handleSortOptionChange(searchOptions[searchByOption])}
+                }
+            >
+            {searchByOption}
             </li>
         );
     }
 
+
+    const [term, setTerm] = useState('')
+    const handleTermChange = ({target}) => {
+        setTerm(target.value)
+    }
+
+    const [location, setLocation] = useState('')
+    const handleLocationChange = ({target}) => {
+        setLocation(target.value)
+    }
+
+    const [sortOption, setSortOption] = useState('best_match')
+    const handleSortOptionChange = (sortOptionUpdate) => {
+        setSortOption(sortOptionUpdate)
+    }
+
+    const handleSearchSubmit = (event) => {
+        event.preventDefault();
+        console.log(`Searching ${term}, ${location}, ${sortOption}`)
+    }
+
     return (
-        <div>
-            <div class="container">
-                <header className="d-flex justify-content-center py-3 searchBarHeader">
-                    <ul className="nav nav-pills">{searchByOptions()}</ul>
+        <div className='searchBarContainer'>
+            <div className='container '>
+                <header className='d-flex justify-content-center py-3 searchBarHeader'>
+                    <ul className='nav nav-pills'>{searchByOptions()}</ul>
                 </header>
             </div>
-            <div class="container">
-                <header className="d-flex justify-content-center py-3">
-                    <input placeholder="Search Businesses" className="searchInput"/>
-                    <input placeholder="Search Location" className="searchInput"/>
+            <form className='container' onSubmit={handleSearchSubmit}>
+                <header className='d-flex justify-content-center py-3'>
+                    <input placeholder='Search Businesses' className='searchInput' onChange={handleTermChange}/>
+                    <input placeholder='Search Location' className='searchInput' onChange={handleLocationChange}/>
                 </header>
-            </div>
+                <div><button type="submit" className="btn btn-primary">Search</button></div>
+            </form>
         </div>
     )
 }
 
-export default searchBar
+export default SearchBar
