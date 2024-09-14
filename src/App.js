6083a86b952169ef06971e395125004ad4b1dbbd
“calaisgalbraith” <calaisgalbraith@gmail.com>
2024-09-14 09:39:22 -0500

    refactored yelp api call on first load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SearchBar from './components/SearchBar/SearchBar'
 import BusinessList from './components/BusinessList/BusinessList'
 import BackToTop from './components/BackToTop/BackToTop';
@@ -8,12 +8,17 @@ import './App.css';
 function App() {
   const [businesses, setBusinesses] = useState([]);
   const searchYelp = async (query, location, sort) => {
+    console.log('search yelp')
     const businessesResults = await Yelp(query, location, sort);
     setBusinesses(businessesResults);
     // if (!suggestions) {
     //   setShowMessage(true);
     // }
   }
+
+  useEffect(() => { // on load, do default search
+    searchYelp("", "US", "best_match")
+  }, []);
 
   return (
     <div className='ravenousContainer'>
