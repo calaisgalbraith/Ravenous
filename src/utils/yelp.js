9bc6c61b1e60313fd2921a9fdb65e53a1b130bd4
“calaisgalbraith” <calaisgalbraith@gmail.com>
2024-09-14 20:52:37 -0500

    removed console messages from yelp.js

--- src/utils/yelp.js
+++ src/utils/yelp.js
@@ -3,12 +3,16 @@ const cors = "https://cors-anywhere.herokuapp.com/";
 const baseUrl = "https://api.yelp.com/v3";
 const searchEndpoint = "/businesses/search";
 
-const Yelp = async (query, location, sort) => {    console.log(location)
+const Yelp = async (query, location, sort) => {
     if (location === '') { // if location is empty, default search to US
         location = 'us'
     }
+    console.log(query)
+    console.log(location)
+    console.log(sort)
 
-    const url = `${cors}${baseUrl}${searchEndpoint}?term=${query}&location=${location}&sort_by=${sort}`;
+    const url = `${cors}${baseUrl}${searchEndpoint}?term=${query}&location=${location}&sort_by=${sort}`
+    console.log(url)
     try {
         const response = await fetch (url,
             {
